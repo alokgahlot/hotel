75553c69619ac981c1c278fa7870a583f84693b1
Alok-Gahlot <alok.gahlot@appinventiv.com>
2022-11-05 06:51:10 +0530

    Navlink implemented

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,44 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function NavBar() {
   return (
     <div>
 
-<nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-       Hotel Kunj Vihari
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-     
-   
-      
-      </ul>
-     
-    </div>
-  </div>
-</nav>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <p className="navbar-brand" >
+            Hotel Kunj Vihari
+          </p>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className="nav-link" aria-current="page" href="#">Home</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" href="#">About Us</NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink className="nav-link" href="#">Contact Us</NavLink>
+              </li>
+              {/* <li className="nav-item">
+          <NavLink className="nav-link" href="#">Cart</NavLink>
+        </li> */}
+              <li className="nav-item">
+                <NavLink className="nav-link" href="#">Login</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" href="#">Registration</NavLink>
+              </li>
+            </ul>
+
+          </div>
+        </div>
+      </nav>
 
     </div>
   )
